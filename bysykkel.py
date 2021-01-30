0edce304bf89461dc014a9bbb9e6c6f459fd9a58
Solveig <solveig.naess@gmail.com>
2021-01-30 21:57:54 +0100

    ascii fix

--- bysykkel.py
+++ bysykkel.py
@@ -44,7 +44,7 @@ try:
     # display data
     table = pandas.DataFrame.from_dict(dict_to_show,
                                        orient='index',
-                                       columns=['ant sykler', 'ant låser'])
+                                       columns=['# bikes', '# docks'])
     print(table)
 
     # reset maximum number of rows
